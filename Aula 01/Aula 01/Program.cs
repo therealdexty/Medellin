9ce654b97477a8f6137a5dd23d2cdd62478403b0
GUSTAVO CAMARGO CORREIA <gustavo.ccorreia1@senacsp.edu.br>
2023-08-09 09:26:22 -0300

    meu primeiro commit

---
+++ Aula 01/Aula 01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+            int numero3;
+            int numero4;
+            int soma;
+
+            Console.Write("Digite o 1º numero:");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("O valor digitado foi:" + numero1);
+
+            Console.Write("Digite o 2º numero:");
+            numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("O valor digitado foi:" + numero2);
+
+            Console.Write("Digite o 3º numero:");
+            numero3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("O valor digitado foi:" + numero3);
+
+            Console.Write("Digite o 4º numero:");
+            numero4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("O valor digitado foi:" + numero4);
+
+            soma = numero1 + numero2 + numero3 + numero4;
+
+            Console.WriteLine("O valor total foi:" + soma);
+
+
+
+            Console.ReadKey(); //Nesse momento o ReadKey serve para o nosso programa não fechar.
+        }
+    }
+}
